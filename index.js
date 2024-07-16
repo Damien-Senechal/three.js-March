fecc8b2f98bd0280d807d0059e3748fff5ebb7b4
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2024-07-16 16:07:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,26 @@ const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 5;
 const scene = new THREE.Scene();
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const geometry = new THREE.IcosahedronGeometry(1.0, 3);
+const geometry = new THREE.IcosahedronGeometry(1, 16);
 const material = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: false });
 
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-const wireMaterial = new THREE.MeshBasicMaterial({ color: 0x927F7E, wireframe: true });
-const wireSphere = new THREE.Mesh(geometry, wireMaterial);
-wireSphere.scale.setScalar(1.001);
-sphere.add(wireSphere);
-
 const light = new THREE.HemisphereLight(0xDEC2C1, 0x765A7E);
 scene.add(light);
 
-function animate(t = 0) {
+function animate() {
     requestAnimationFrame(animate);
-    sphere.rotation.y = t*0.0001;
-    sphere.rotation.x = t*0.0001;
+    sphere.rotation.y += 0.001;
+    sphere.rotation.x += 0.002;
     renderer.render(scene, camera);
     controls.update();
 }
