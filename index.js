57b848f43731026fd003a37627ce427b75f027cd
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2024-07-16 18:52:42 +0200

    add march

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import getStarfield from "./src/getStarfield.js";
+import { getFresnelMat } from "./src/getFresnelMat.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-const fov = 75;
-const aspect = w / h;
-const near = 0.1;
-const far = 10;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 5;
-const scene = new THREE.Scene();
+const marchGroup = new THREE.Group();
+marchGroup.rotation.z = -23.4 * (Math.PI / 180);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.03;
+const ctrls = new OrbitControls(camera, renderer.domElement);
+ctrls.enableDamping = true;
 
-const geometry = new THREE.IcosahedronGeometry(1, 16);
-const material = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: false });
+const detail = 6;
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+const loader = new THREE.TextureLoader();
+const material = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/mars_1k_color.jpg"),
+});
+const marchMesh = new THREE.Mesh(geometry, material);
+marchGroup.add(marchMesh);
 
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./textures/mars_1k_color.jpg"),
+  transparent: true,
+  opacity: 0.1,
+  blending: THREE.AdditiveBlending,
+});
+const lightMesh = new THREE.Mesh(geometry, lightsMat);
+marchGroup.add(lightMesh);
 
-const light = new THREE.HemisphereLight(0xDEC2C1, 0x765A7E);
-scene.add(light);
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+marchGroup.add(glowMesh);
+
+const starfield = getStarfield({ numStars: 2000 });
+scene.add(starfield);
+
+const sun = new THREE.DirectionalLight(0xffffff);
+sun.position.set(-2, 0.5, 1.5);
+scene.add(sun);
+
+scene.add(marchGroup);
 
 function animate() {
-    requestAnimationFrame(animate);
-    sphere.rotation.y += 0.001;
-    sphere.rotation.x += 0.002;
-    renderer.render(scene, camera);
-    controls.update();
+  requestAnimationFrame(animate);
+  marchGroup.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.001);
+  renderer.render(scene, camera);
+  ctrls.update();
 }
 
 animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
